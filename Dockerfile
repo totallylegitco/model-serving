ebe9f3758234915e5a48ae4b47080b1a2a3516be
Holden Karau <holden@pigscanfly.ca>
2023-07-29 18:08:59 -0700

    Maybe install something. Probably not yet though.

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM holdenk/ray-x86-and-l4t:c21092023
 RUN sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y libpq-dev libopenblas-dev
 # Setup the dependencies in advance
 RUN pip install torch ${PIP_EXTRA}
-COPY requirements.txt /tmp/req.txt
-# See https://github.com/python/typing/issues/573
-RUN pip uninstall -y typing && CMAKE_ARGS="-DGGML_OPENBLAS=ON" pip install -U -r /tmp/req.txt ${PIP_EXTRA}
+COPY requirements.txt /tmp/
+COPY setup.sh ./
+RUN ./setup.sh
 COPY model_setup.py ./
 RUN python model_setup.py
---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# On Jetson AGX we need to add /usr/local/cuda/bin to our path
+if [ -f /usr/local/cuda/bin/nvcc ]; then
+  export PATH=$PATH:/usr/local/cuda/bin
+fi
+
+# See https://github.com/python/typing/issues/573
+pip uninstall typing
+
+# We need to install pybind11 before deepspeed because it is not listed as a depdency.
+pip install pybind11[global]
+if [ ! -d apex ]; then
+  git clone https://github.com/NVIDIA/apex
+fi
+cd apex
+sudo pip install -U pip
+pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" --config-settings "--build-option=--deprecated_fused_adam" ./
+cd ..
+
+CU_MINOR=$(nvcc --version |grep "cuda_" |cut -d "_" -f 2 |cut -d "." -f 2)
+pip install "torch" --index-url https://download.pytorch.org/whl/cu11${CU_MINOR} || pip install torch
+
+pip install ninja hjson py-cpuinfo
+
+DS_BUILD_CPU_ADAM=1 DS_BUILD_SPARSE_ATTN=0 DS_BUILD_FUSED_ADAM=1 pip install "git+https://github.com/microsoft/deepspeed.git#" --global-option="build_ext" --global-option="-j16"
+
+CMAKE_ARGS="-DGGML_OPENBLAS=ON" pip3 install -U -r /tmp/requirements.txt --extra-index-url https://download.pytorch.org/whl/cu11${CU_MINOR}
